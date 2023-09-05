fdbef3585c13596cdad226b03fd9606ffd196444
unknown <dezsoszabolcs46@gmail.com>
2023-09-05 15:40:44 +0300

    Favicon and activity cards created

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
